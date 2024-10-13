02e90f9492349c9bf3251139fe99eb4dddf4e4ce
carlock2022 <114795834+carlock2022@users.noreply.github.com>
2024-10-13 16:29:22 -0400

    Update useServerAttributes.js

--- src/common/attributes/useServerAttributes.js
+++ src/common/attributes/useServerAttributes.js
@@ -89,6 +89,110 @@ export default (t) => useMemo(() => ({
     name: t('servergmcontacto'),
     type: 'string',
   },
+  serverwscontacto: {
+    name: t('serverwscontacto'),
+    type: 'string',
+  },
+  'ui.disableLoginLanguage': {
+    name: t('attributeUiDisableLoginLanguage'),
+    type: 'boolean',
+  },
+  disableShare: {
+    name: t('serverDisableShare'),
+    type: 'boolean',
+  },
+}), [t]);
+import { useMemo } from 'react';
+
+export default (t) => useMemo(() => ({
+  support: {
+    name: t('settingsSupport'),
+    type: 'string',
+  },
+  title: {
+    name: t('serverName'),
+    type: 'string',
+  },
+  description: {
+    name: t('serverDescription'),
+    type: 'string',
+  },
+  logo: {
+    name: t('serverLogo'),
+    type: 'string',
+  },
+  logoInverted: {
+    name: t('serverLogoInverted'),
+    type: 'string',
+  },
+  colorPrimary: {
+    name: t('serverColorPrimary'),
+    type: 'string',
+    subtype: 'color',
+  },
+  colorSecondary: {
+    name: t('serverColorSecondary'),
+    type: 'string',
+    subtype: 'color',
+  },
+  colorGeneral: {
+    name: t('serverColorGeneral'),
+    type: 'string',
+    subtype: 'color',
+  },
+  colorText: {
+    name: t('serverColorText'),
+    type: 'string',
+    subtype: 'color',
+  },
+  disableChange: {
+    name: t('serverChangeDisable'),
+    type: 'boolean',
+  },
+  darkMode: {
+    name: t('settingsDarkMode'),
+    type: 'boolean',
+  },
+  termsUrl: {
+    name: t('userTerms'),
+    type: 'string',
+  },
+  privacyUrl: {
+    name: t('userPrivacy'),
+    type: 'string',
+  },
+  totpEnable: {
+    name: t('settingsTotpEnable'),
+    type: 'boolean',
+  },
+  totpForce: {
+    name: t('settingsTotpForce'),
+    type: 'boolean',
+  },
+  serviceWorkerUpdateInterval: {
+    name: t('settingsServiceWorkerUpdateInterval'),
+    type: 'number',
+  },
+  serverfbcontacto: {
+    name: t('serverfbcontacto'),
+    type: 'string',
+  },
+  servermedia: {
+    name: t('servermedia'),
+    type: 'string',
+  },
+  serverapple: {
+    name: t('serverapple'),
+    type: 'string',
+  },
+  servergoogle: {
+    name: t('servergoogle'),
+    type: 'string',
+  },
+  servergmcontacto: {
+    name: t('servergmcontacto'),
+    type: 'string',
+  },
   'ui.disableLoginLanguage': {
     name: t('attributeUiDisableLoginLanguage'),
     type: 'boolean',
