0f2d6bed1ff0c08b1feeb6823c26fa22a3085bc6
carlock2022 <114795834+carlock2022@users.noreply.github.com>
2024-10-13 13:53:12 -0400

    Update LoginLayout.jsx
    añadir video con servermedia

--- src/login/LoginLayout.jsx
+++ src/login/LoginLayout.jsx
@@ -2,28 +2,15 @@ import React from 'react';
 import { useMediaQuery, Paper } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import { useTheme } from '@mui/material/styles';
-import LogoImage from './LogoImage';
+import { useSelector } from 'react-redux'; // Asegúrate de importar useSelector
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     height: '100%',
   },
-  sidebar: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    background: theme.palette.primary.main,
-    paddingBottom: theme.spacing(5),
-    width: theme.dimensions.sidebarWidth,
-    [theme.breakpoints.down('lg')]: {
-      width: theme.dimensions.sidebarWidthTablet,
-    },
-    [theme.breakpoints.down('sm')]: {
-      width: '0px',
-    },
-  },
   paper: {
+    position: 'relative', // Necesario para que la posición absoluta funcione correctamente
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
@@ -31,8 +18,21 @@ const useStyles = makeStyles((theme) => ({
     flex: 1,
     boxShadow: '-2px 0px 16px rgba(0, 0, 0, 0.25)',
     [theme.breakpoints.up('lg')]: {
-      padding: theme.spacing(0, 25, 0, 0),
+      padding: theme.spacing(0, 0, 0, 0),
     },
+  },
+  videoContainer: {
+    position: 'absolute',
+    width: '100%',
+    height: '120%',
+    top: 0,
+    left: 0,
+    overflow: 'hidden',
+  },
+  video: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
   },
   form: {
     maxWidth: theme.spacing(52),
@@ -45,12 +45,18 @@ const LoginLayout = ({ children }) => {
   const classes = useStyles();
   const theme = useTheme();
 
+  // Mueve el uso de useSelector dentro del componente funcional
+  const servermedia = useSelector((state) => state.session.server.attributes?.servermedia);
+
   return (
     <main className={classes.root}>
-      <div className={classes.sidebar}>
-        {!useMediaQuery(theme.breakpoints.down('lg')) && <LogoImage color={theme.palette.secondary.contrastText} />}
-      </div>
       <Paper className={classes.paper}>
+        <div className={classes.videoContainer}>
+          <video autoPlay loop muted className={classes.video}>
+            <source src={`${servermedia}/fondogps.mp4`} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
         <form className={classes.form}>
           {children}
         </form>
